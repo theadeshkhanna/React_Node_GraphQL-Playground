d7498841fccc7a251c21eb45f6afb996835edf88
the_adeshkhanna <adeshkhanna271@gmail.com>
2020-09-13 21:50:41 +0530

    added graphQL schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,6 @@
 const graphql = require("graphql");
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
 const BookType = new GraphQLObjectType({
     name: "Book",
@@ -17,4 +17,25 @@ const BookType = new GraphQLObjectType({
             type: GraphQLString
         }
     })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        book: {
+            type: BookType,
+            args: {
+                id: {
+                    type: GraphQLString
+                }
+            },
+            resolve(parent, args) {
+                // code to get data
+            }
+        },
+    }
+});
+
+module.exports = new GraphQLSchema({
+    root: RootQuery
 });
