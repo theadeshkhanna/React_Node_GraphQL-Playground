8616f1c9b902a351a0f5791441a636d60a39ef7c
the_adeshkhanna <adeshkhanna271@gmail.com>
2020-09-22 11:47:10 +0530

    added refetch queries

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { graphql } from "react-apollo";
-import { getAuthorsQuery, addBookMutation } from "../queries/queries";
+import { getAuthorsQuery, addBookMutation, getBooksQuery } from "../queries/queries";
 import { flowRight as compose } from 'lodash';
 
 class AddBook extends Component {
@@ -38,7 +38,12 @@ class AddBook extends Component {
         name: this.state.name,
         genre: this.state.genre,
         authorId: this.state.authorId
-      }
+      },
+      refetchQueries: [
+        {
+          query: getBooksQuery
+        }
+      ]
     });
   }
 
