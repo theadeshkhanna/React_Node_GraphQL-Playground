19d21ec7035a1c7ddfcc0d9f68fd26e799c7c3bb
the_adeshkhanna <adeshkhanna271@gmail.com>
2020-09-22 12:26:15 +0530

    added book detail component

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import { graphql } from "react-apollo";
 import { getBooksQuery } from "../queries/queries";
+import BookDetails from './BookDetails';
 
 class BookList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: null
+    }
+  }
   
   displayBooks(){
     var data = this.props.data;
@@ -15,7 +23,9 @@ class BookList extends Component {
     } else {
       return data.books.map((book) => {
           return (
-            <li key={ book.id }>{ book.name }</li>
+            <li key={ book.id } onClick={(e) => {this.setState({selected: book.id})}}>
+              { book.name }
+            </li>
           );
         });
     }
@@ -27,6 +37,7 @@ class BookList extends Component {
           <ul id="book-list">
               { this.displayBooks() }
           </ul>
+          <BookDetails bookId={this.state.selected}/>
       </div>
     );
   }
