3dd87215e033f3e3f0a8a2aa80b0f78e8480d5f6
the_adeshkhanna <adeshkhanna271@gmail.com>
2020-09-22 11:28:13 +0530

    added Book Mutation

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { graphql } from "react-apollo";
-import { getAuthorsQuery } from "../queries/queries";
+import { getAuthorsQuery, addBookMutation } from "../queries/queries";
+import { flowRight as compose } from 'lodash';
 
 class AddBook extends Component {
 
@@ -14,7 +15,7 @@ class AddBook extends Component {
   }
 
   displayAuthors() {
-      var data = this.props.data;
+      var data = this.props.getAuthorsQuery;
       if (data.loading) {
           return (
             <option disabled>
@@ -32,7 +33,7 @@ class AddBook extends Component {
 
   submitForm(e) {
     e.preventDefault();
-    console.log(this.state);
+    this.props.addBookMutation();
   }
 
   render() {
@@ -59,4 +60,7 @@ class AddBook extends Component {
   }
 }
 
-export default graphql(getAuthorsQuery)(AddBook);
+export default compose(
+  graphql(getAuthorsQuery, { name: "getAuthorsQuery" }),
+  graphql(addBookMutation, { name: "addBookMutation" })
+)(AddBook);
